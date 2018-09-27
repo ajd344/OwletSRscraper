a04573d2caf30de79c0c5436c16557591e8aad7b
unknown <ajd344@drexel.edu>
2018-09-27 14:22:05 -0400

    Modified to fit new sheet format

--- scraper.py
+++ scraper.py
@@ -2,6 +2,7 @@ import requests
 import csv
 from bs4 import BeautifulSoup
 import re
+import datetime
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
@@ -9,29 +10,33 @@ scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/au
 creds = ServiceAccountCredentials.from_json_keyfile_name('C:\Users\Andrew\Documents\client-secret.json', scope)
 client = gspread.authorize(creds)
 
-sheet = client.open("OWLET Master Spreadsheet").worksheet("Map Count Code")
+sheet = client.open("OWLET Master Spreadsheet").worksheet("Stage 4 Rosters")
 
 listOfHashes = sheet.get_all_values()
 
 finished = {}
 btags = []
+format = re.compile(".*[#]*\d")
 
-for row in listOfHashes:
-    btags.append(row)
+for row in  listOfHashes:
+    if format.match(row[2]):
+        btags.append(row)
+col = [i for i, n in enumerate(btags[0]) if n == ''][1]
 
-for name in btags:
+for player in btags:
+    name = player[2]
     if (len(name) > 0):
-        tag = name[0]
+        tag = name
         cell = sheet.find(tag)
         tag = tag.replace('#', '-')
         tag = tag.replace(" ", "")
-        player = 'https://playoverwatch.com/en-us/career/pc/' + tag
+        link = 'https://playoverwatch.com/en-us/career/pc/' + tag
+
         try:
-            page = requests.get(player)
+            page = requests.get(link)
             soup = BeautifulSoup(page.content, 'html.parser')
             ret = soup.find(class_="competitive-rank").get_text()
         except:
-            ret = ""
-        finished[name[0]] = ret
-        sheet.update_cell(cell.row, 5, ret)
-
+            ret = "ND"
+        finished[name] = ret
+        sheet.update_cell(cell.row, col+1, ret)
